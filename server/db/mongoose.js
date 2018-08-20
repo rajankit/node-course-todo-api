13c16f5333e2ace0ea9046b59bca6f9aa1ab417b
rajankit <ankit.raj.min14@itbhu.ac.in>
2018-08-21 02:02:43 +0530

    set up different test and development env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // 'mongodb://ankitrj:ankit1430@ds125872.mlab.com:25872/ankitrj-todo-api' ||
-mongoose.connect(process.env.MONGODB_URI ||  'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
