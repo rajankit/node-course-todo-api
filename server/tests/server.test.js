d838eb6e4a0d473fa7734c84dad9cbbe79d1b156
rajankit <ankit.raj.min14@itbhu.ac.in>
2018-08-22 13:25:07 +0530

    Add test for POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -237,3 +237,47 @@ describe('POST /users', () => {
     });
 
 });
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({email : users[1].email, password : users[1].password})
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toBeTruthy();
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).toInclude({
+                    "access": "auth",
+                    "token": res.headers['x-auth']
+                });
+                done();
+            }).catch((e) => done(e));
+        });
+
+    });
+
+    it('should reject invalid credentials', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({email : users[1].email, password : users[1].password + '23'})
+        .expect(400)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toBeFalsy();
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+});
