d44c945dc667ab2506e17e1280025ab753c354a3
rajankit <ankit.raj.min14@itbhu.ac.in>
2018-08-22 12:48:35 +0530

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -100,6 +100,8 @@ app.patch('/todos/:id', (req, res) => {
 
 // FOR USERS
 
+
+// for POST /users SIGNIN
 app.post('/users',(req, res) => {
     var body = _.pick(req.body,['email', 'password']);
     var user = new User(body);
@@ -112,6 +114,19 @@ app.post('/users',(req, res) => {
         res.status(400).send(e);
     });
 });
+
+// for POST /users/login
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body,['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 
 app.get('/users/me',authenticate, (req, res) => {
     res.send(req.user);
