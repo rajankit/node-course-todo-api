4916e7498e051b31a61bd953876b888f53474f77
rajankit <ankit.raj.min14@itbhu.ac.in>
2018-08-22 13:40:14 +0530

    Add DELETE  /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -56,6 +56,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull : {
+            tokens:{ token }
+        }
+    });
+};
+
 
 //Model Methods
 
